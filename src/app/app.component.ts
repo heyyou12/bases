d4de8612f8ba4d77ae16682db493308214867916
Manuel Restrepo <manolo.1217@hotmail.com>
2022-12-14 21:20:06 -0500

    funcion contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  titulo: string='Contador App';
+
+ numero: number = 10;
+
+ acumular(valor:number) {
+    return this.numero += valor;
+ }
+  
 }
